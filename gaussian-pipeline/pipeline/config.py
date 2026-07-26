eb92a5f48ba97ebb4233d60a0bea6cf370db99cf
Shivansh Anand <22je0910@iitism.ac.in>
2026-07-26 11:52:00 +0530

    1

---
+++ gaussian-pipeline/pipeline/config.py
@@ -0,0 +1,59 @@
+from pathlib import Path
+
+# ==========================================================
+# ROOTS
+# ==========================================================
+
+ROOT = Path("/kaggle/working")
+
+PROJECT_ROOT = ROOT / "synth_ds_creation"
+
+GRAPHDECO = ROOT / "gaussian-splatting"
+
+WORKSPACE = ROOT / "workspace"
+
+# ==========================================================
+# DATA
+# ==========================================================
+
+DATASET_ROOT = Path("/kaggle/input")
+
+OUTPUT_ROOT = Path("/kaggle/working/output")
+
+CHECKPOINT_ROOT = OUTPUT_ROOT / "checkpoints"
+
+# ==========================================================
+# PARAMETERS
+# ==========================================================
+
+FPS = 2
+
+IMAGE_EXTENSION = ".png"
+
+IMAGE_SIZE = 1600 #specify width height of img
+
+RANDOM_SEED = 42 #initial value given to a random number generator
+
+for folder in [
+
+    WORKSPACE,
+
+    OUTPUT_ROOT,
+
+    CHECKPOINT_ROOT
+
+]:
+
+    folder.mkdir(parents=True, exist_ok=True)
+
+
+
+
+# ==========================================================
+# EXTERNAL REPOSITORIES
+# ==========================================================
+
+GRAPHDECO_REPO = "https://github.com/graphdeco-inria/gaussian-splatting.git"
+
+# Temporary until we pin a tested commit
+GRAPHDECO_COMMIT = "main"
